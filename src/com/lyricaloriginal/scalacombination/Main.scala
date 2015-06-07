74e8adee20713bd7a6079c29395d49c1085bc5dc
LyricalMaestro <lyricalmaestro000@gmail.com>
2015-06-07 23:52:05 +0900

    動的計画法を用いるようにした。

--- src/com/lyricaloriginal/scalacombination/Main.scala
+++ src/com/lyricaloriginal/scalacombination/Main.scala
@@ -1,21 +1,31 @@
 package com.lyricaloriginal.scalacombination
 
 object Main {
+
   def main(args: Array[String]) {
-    val n = 40;
-    val m = 15;
+    val n = 35;
+    val m = 30;
     val answer = combination(n, m);
     println("Combination(" + n + ", " + m + ") = " + answer);
   }
 
   def combination(n: Int, m: Int): Int = {
     //TODO n <0, m < 0, n < mの時の例外処理を追加する。
+    val combiValues = Array.ofDim[Int](n, m);
+    return combination(n, m, combiValues);
+  }
+
+  def combination(n: Int, m: Int, combiValues: Array[Array[Int]]): Int = {
     if (m == 0 || m == n) {
       return 1;
     } else if (m == 1 || n - 1 == m) {
       return n;
-    } else {
-      return combination(n - 1, m - 1) + combination(n - 1, m);
+    } else if(combiValues(n - 1)(m - 1) > 0){
+      return combiValues(n - 1)(m - 1);
+    }else{
+      val value = combination(n - 1, m - 1, combiValues) + combination(n - 1, m, combiValues);
+      combiValues(n - 1)(m - 1) = value;
+      return value;
     }
   }
 }
